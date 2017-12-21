e1c420dcada0a1af39f00a7e1e857e3311e5a9ad
niawjunior <pasupol@dome.cloud>
2017-12-21 13:36:56 +0700

    create  component ShowDoneToggle

---
+++ src/components/ShowDoneToggle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ShowDoneToggle = ({showDone, handleChangeShowDone})=>{
+    return(
+        <div>
+            <label>Show completed item?</label>
+            <input type="checkbox" checked={showDone} onChange={handleChangeShowDone} />
+        </div>
+    )
+};
+
+const mapStateToProps = ({showDone})=>({showDone});
+
+const mapDispatchToProps = (dispatch)=>({
+    handleChangeShowDone(e){
+        dispatch({type:"SET_SHOW_DONE", showDone:e.target.checked})
+    }
+});
+export default connect(mapStateToProps,mapDispatchToProps)(ShowDoneToggle);
