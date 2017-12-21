16db1d75863db7306d07bdf0961174a4e2252018
niawjunior <pasupol@dome.cloud>
2017-12-21 10:53:32 +0700

    :sparkles:

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import TodoList from './TodoList';
+import TodoInput from './TodoInput';
+
 const App = () =>(
   <div>
     <h1>hello</h1>
     <TodoList/>
+    <TodoInput/>
   </div>
 )
 
